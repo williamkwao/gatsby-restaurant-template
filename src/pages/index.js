add7b5c1be0bfcdb59604608e83a0c5e001ea33a
William Kwao <williamkwao@gmail.com>
2020-01-18 21:26:42 -0500

    added about section to landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,107 @@ import SEO from "../components/seo"
 import styled from "styled-components"
 import landingImg from "../images/landingimage.jpg"
 import MenuComponent from "../components/menu"
+import chefImage from "../images/chef.jpg"
 
-const ActionButton = styled.button`
+const IndexPage = () => (
+  <Layout>
+    <HomeStyles>
+      <div className="landing">
+        <div className="landing-box">
+          <h2>We serve good food</h2>
+          <p>
+            Authentic food sourced from the best location with advanced non GMO
+            methods
+          </p>
+          <Link to="/menu">
+            <ActionButton>View our menu</ActionButton>
+          </Link>
+        </div>
+      </div>
+      <div className="res-info">
+        <div className="res-info-item">
+          <h3 className="title">Address</h3>
+          <p>98 West 21th Street, Suite 721 New York NY 10016</p>
+        </div>
+        <div className="res-info-item">
+          <h3 className="title">Opening Time</h3>
+          <p>Monday - Sunday 8am - 9pm</p>
+        </div>
+        <div className="res-info-item">
+          <h3 className="title">Phone</h3>
+          <p>+ 001 234 567 + 001 234 567</p>
+        </div>
+        <div className="res-info-item">
+          <h3 className="title">Email</h3>
+          <p>info@domain.com luto@email.com</p>
+        </div>
+      </div>
+      <AboutSection>
+        <div className="text-section">
+          <h3>Welcome to Alo Vietnam</h3>
+          <h2>Authenthic taste of Vietnam in Herndon</h2>
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts. Separated they
+            live in Bookmarksgrove right at the coast of the Semantics.
+          </p>
+        </div>
+        <div className="img-div">
+          <img src={chefImage}></img>
+        </div>
+      </AboutSection>
+      <MenuComponent />
+    </HomeStyles>
+  </Layout>
+)
+
+var AboutSection = styled.section`
+  padding: 60px 15px;
+  max-width: 800px;
+  margin: auto;
+  margin-top: 44px;
+  margin-bottom: 44px;
+  img {
+    height: 450px;
+    margin: auto;
+  }
+  .text-section {
+    h3 {
+      color: #969696;
+      font-family: Lora;
+      font-size: 18px;
+      font-weight: 400;
+      font-style: italic;
+      line-height: 32px;
+    }
+
+    h2 {
+      font-family: Lora, Georgia, serif;
+      font-style: normal;
+      line-height: 62px;
+      font-size: 44px;
+      color: #404044;
+    }
+    p {
+      font-family: Poppins, Arial, sans-serif;
+      color: #7d7d7d;
+      font-size: 15px;
+      line-height: 27px;
+    }
+  }
+  .img-div {
+    text-align: center;
+  }
+  @media (min-width: 1025px) {
+    display: grid;
+    grid-template-columns: 50% 50%;
+
+    .img-div {
+      text-align: right;
+    }
+  }
+`
+var ActionButton = styled.button`
   background: #ff6107;
   font-style: normal;
   line-height: 17px;
@@ -23,7 +122,7 @@ const ActionButton = styled.button`
   text-transform: uppercase;
   padding: 18px 36px !important;
 `
-const HomeStyles = styled.div`
+var HomeStyles = styled.div`
   font-family: Lora !important;
   min-height: 100vh;
   .landing {
@@ -71,42 +170,4 @@ const HomeStyles = styled.div`
     }
   }
 `
-const IndexPage = () => (
-  <Layout>
-    <HomeStyles>
-      <div className="landing">
-        <div className="landing-box">
-          <h2>We serve good food</h2>
-          <p>
-            Authentic food sourced from the best location with advanced non GMO
-            methods
-          </p>
-          <Link to="/menu">
-            <ActionButton>View our menu</ActionButton>
-          </Link>
-        </div>
-      </div>
-      <div className="res-info">
-        <div className="res-info-item">
-          <h3 className="title">Address</h3>
-          <p>98 West 21th Street, Suite 721 New York NY 10016</p>
-        </div>
-        <div className="res-info-item">
-          <h3 className="title">Opening Time</h3>
-          <p>Monday - Sunday 8am - 9pm</p>
-        </div>
-        <div className="res-info-item">
-          <h3 className="title">Phone</h3>
-          <p>+ 001 234 567 + 001 234 567</p>
-        </div>
-        <div className="res-info-item">
-          <h3 className="title">Email</h3>
-          <p>info@domain.com luto@email.com</p>
-        </div>
-      </div>
-      <MenuComponent />
-    </HomeStyles>
-  </Layout>
-)
-
 export default IndexPage
